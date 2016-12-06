0e3c702c5376eccbf03f7a8cddcfe9d8c20b65c8
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-12-06 16:47:38 -0600

    Finish day 6

---
+++ day_6.rb
@@ -0,0 +1,31 @@
+require_relative './day_6_data.rb'
+
+# get the data
+strings = Day6Data::STRINGS
+# initialize the message array for the max character in each column
+max_message = []
+# initialize the message array for the min character in each column
+min_message = []
+
+# for each column index
+(0..7).each do |index|
+  # initialize hash -- local to the column
+  hash = {}
+  # each string
+  strings.each do |string|
+    # get the character from the string
+    char = string[index]
+    # if the character already exists in the hash increment the count
+    # otherwise initialize the character in the hash
+    hash.key?(char) ? hash[char] += 1 : hash.merge!(char.to_s => 1)
+  end
+
+  # find the max value in the hash and send that character to the max message
+  max_message.push(hash.max_by { |_key, value| value }[0])
+  # find the min value in the hash and send that character to the min message
+  min_message.push(hash.min_by { |_key, value| value }[0])
+end
+
+# display the results
+p "MAX MESSAGE: #{max_message.join('')}"
+p "MIN MESSAGE: #{min_message.join('')}"
