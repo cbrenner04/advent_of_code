c188d3b8cde5540eb66ea78760c5cd8b9efee77b
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-12-25 23:27:41 -0500

    Continue work on day 10

--- day_10.rb
+++ day_10.rb
@@ -26,30 +26,29 @@ outputs = {}
 # rubocop:disable BlockNesting
 while done_instructions.length < data.length
   unless done_instructions.include?(instructions_index)
-    instruction = data[instructions_index]
-    if instruction[0..2] == "bot"
-      inst_array = instruction.split
-      first_hash = inst_array[5] == "bot" ? bots : outputs
-      second_hash = inst_array[10] == "bot" ? bots : outputs
-      bot = bots[inst_array[1]]
+    instruction_array = data[instructions_index].split
+    keyword = instruction_array[0]
+    if keyword == "bot"
+      bot_name = instruction_array[1]
+      first_hash = instruction_array[5] == "bot" ? bots : outputs
+      second_hash = instruction_array[10] == "bot" ? bots : outputs
+      bot = bots[bot_name]
       if bot&.length == 2
-        give_values(inst_array[6], bot.min, first_hash)
-        give_values(inst_array[11], bot.max, second_hash)
-        bots.delete(inst_array[1])
+        give_values(instruction_array[6], bot.min, first_hash)
+        give_values(instruction_array[11], bot.max, second_hash)
         done_instructions.push(instructions_index)
       end
-    elsif instruction[0..4] == "value"
-      inst_array = instruction.split
-      value = inst_array[1]
-      bot = inst_array[5]
+    elsif keyword == "value"
+      value = instruction_array[1]
+      bot = instruction_array[5]
       give_values(bot, value, bots)
       done_instructions.push(instructions_index)
     end
   end
 
   instructions_index = increment(instructions_index, data)
-  break if bots.any? { |b| b[1] == %w(61 17) }
+  break if bots.any? { |b| b[1].sort == %w(17 61) }
 end
 # rubocop:enable BlockNesting
 
-p bots
+bots.each { |b| p b[0] if b[1].sort == %w(17 61) }
