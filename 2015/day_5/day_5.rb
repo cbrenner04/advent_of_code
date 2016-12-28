15a8bc41f9d3cb7920234e5fa1a3bed8ffdadb23
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-12-28 14:11:03 -0600

    Finish 2015 day 5

---
+++ 2015/day_5/day_5.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+data = []
+File.open("day_5_data.txt", "r") { |f| f.each_line { |l| data.push l[0..-2] } }
+count = 0
+
+data.each do |datum|
+  next if datum.include?("ab") || datum.include?("cd") ||
+          datum.include?("pq") || datum.include?("xy")
+  next unless datum.scan(/[aeoui]/).count >= 3
+  repeated = false
+  datum.each_char.with_index { |c, i| repeated = true if c == datum[i + 1] }
+  next unless repeated == true
+  count += 1
+end
+
+p count
---
+++ 2015/day_5/day_5_b.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+data = []
+File.open("day_5_data.txt", "r") { |f| f.each_line { |l| data.push l[0..-2] } }
+count = 0
+
+data.each do |datum|
+  repeated = false
+  last_index = datum.length - 1
+  (0..last_index).each do |i|
+    sub = datum[i..i + 1]
+    (i + 2..last_index).each { |j| repeated = true if sub == datum[j..j + 1] }
+  end
+  next unless repeated == true
+
+  repeated = false
+  datum.each_char.with_index { |c, i| repeated = true if c == datum[i + 2] }
+  next unless repeated == true
+  count += 1
+end
+
+p count
