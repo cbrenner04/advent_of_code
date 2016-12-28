cc7250d62df389085a5b524a67a60880c5de948e
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-12-28 14:44:04 -0600

    Finish day 6

---
+++ 2015/day_6/day_6.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+ROWS = 1000
+COLUMNS = 1000
+
+data = []
+File.open("day_6_data.txt", "r") do |file|
+  file.each_line do |line|
+    data.push line[0..-2]
+      .gsub("turn on", "turn-on")
+      .gsub("turn off", "turn-off")
+      .split(" ")
+  end
+end
+
+matrix = []
+
+ROWS.times do
+  column = []
+  COLUMNS.times { column.push(false) }
+  matrix.push(column)
+end
+
+data.each do |datum|
+  command = datum[0]
+  start = datum[1].split(",")
+  start_x = start[0].to_i
+  start_y = start[1].to_i
+  finish = datum[3].split(",")
+  finish_x = finish[0].to_i
+  finish_y = finish[1].to_i
+  if command == "turn-on" || command == "turn-off"
+    value = command == "turn-on" ? true : false
+    (start_x..finish_x).each do |x_index|
+      (start_y..finish_y).each do |y_index|
+        matrix[x_index][y_index] = value
+      end
+    end
+  elsif command == "toggle"
+    (start_x..finish_x).each do |x_index|
+      (start_y..finish_y).each do |y_index|
+        matrix[x_index][y_index] = if matrix[x_index][y_index] == false
+                                     true
+                                   else
+                                     false
+                                   end
+      end
+    end
+  end
+end
+
+count = 0
+
+matrix.each { |s| s.each { |a| count += 1 if a == true } }
+
+p count
---
+++ 2015/day_6/day_6_b.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+ROWS = 1000
+COLUMNS = 1000
+
+data = []
+File.open("day_6_data.txt", "r") do |file|
+  file.each_line do |line|
+    data.push line[0..-2]
+      .gsub("turn on", "turn-on")
+      .gsub("turn off", "turn-off")
+      .split(" ")
+  end
+end
+
+matrix = []
+
+ROWS.times do
+  column = []
+  COLUMNS.times { column.push(0) }
+  matrix.push(column)
+end
+
+data.each do |datum|
+  command = datum[0]
+  start = datum[1].split(",")
+  start_x = start[0].to_i
+  start_y = start[1].to_i
+  finish = datum[3].split(",")
+  finish_x = finish[0].to_i
+  finish_y = finish[1].to_i
+  value = if command == "turn-on"
+            1
+          elsif command == "turn-off"
+            -1
+          elsif command == "toggle"
+            2
+          end
+  (start_x..finish_x).each do |x_index|
+    (start_y..finish_y).each do |y_index|
+      matrix[x_index][y_index] += value
+      matrix[x_index][y_index] = 0 if (matrix[x_index][y_index]).negative?
+    end
+  end
+end
+
+count = 0
+
+matrix.each { |s| s.each { |a| count += a } }
+
+p count
