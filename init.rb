00f0b88a06634600ab3c7b76f51dede036e3741f
cbrenner04 <cbrenner265@gmail.com>
2018-12-02 13:22:12 -0600

    Update initialization so external dependencies not needed

--- init.rb
+++ init.rb
@@ -1,7 +1,30 @@
-require "highline/import"
+# frozen_string_literal: true
 
-year = ask "Year?"
-day = ask "Day?"
+require "optparse"
+
+year = nil
+day = nil
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: init.rb [options]"
+
+  parser.on("-h", "--help", "Show this help message") do
+    puts parser
+  end
+
+  parser.on("-y", "--year YEAR", "The year of the puzzle.") do |y|
+    raise "Must have year" unless y
+    year = y
+  end
+
+  parser.on("-d", "--day DAY", "The day of the puzzle.") do |d|
+    raise "Must have day" unless d
+    day = d
+  end
+end.parse!
+
+raise "Must have YEAR and DAY; use -h for help" unless day && year
+
 dir = "#{year}/day_#{day}"
 
 system "mkdir #{dir}"
