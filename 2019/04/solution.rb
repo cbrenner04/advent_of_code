43129c5932f1dd1de43878cdc396bf228908211d
Christopher Brenner <cbrenner265@gmail.com>
2019-12-05 10:22:40 -0600

    Some day 4 clean up

--- 2019/04/solution.rb
+++ 2019/04/solution.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
-# this is a pretty lame solution
-
 def good_pass(pass)
-  local_pass = pass.to_s
-  good_length = local_pass.length == 6
+  good_length = pass.length == 6
   return false unless good_length
-  has_double = local_pass.scan(/(.)\1/).any?
+  has_double = pass.scan(/(.)\1/).any?
   return false unless has_double
-  array = local_pass.split("")
+  array = pass.split("")
   decreases = array.sort != array
   return false if decreases
   true
 end
 
+# double in previous good pass can't be part of a longer string of same digit
+# therefore if there are no triples the pass is still good
+# or if there are triples then there needs to be another double for a good pass
 def part_two(pass)
-  local_pass = pass.to_s
-  triples = local_pass.scan(/(.)\1\1/).flatten.uniq
-  doubles = local_pass.scan(/(.)\1/).flatten.uniq
-  (triples.empty? && doubles.any?) ||
+  triples = pass.scan(/(.)\1\1/)
+  doubles = pass.scan(/(.)\1/)
+  triples.empty? ||
     (triples.any? && (doubles - triples).any? && (triples - doubles).empty?)
 end
 
 bounds = INPUT.split("-").map(&:to_i)
 range = (bounds.first..bounds.last)
 
-passes_with_doubles = range.select { |pass| pass if good_pass(pass) }
+passes_with_doubles = range.select { |pass| good_pass(pass.to_s) }
 puts passes_with_doubles.length
-puts passes_with_doubles.select { |pass| pass if part_two(pass) }.length
+puts passes_with_doubles.select { |pass| part_two(pass.to_s) }.length
