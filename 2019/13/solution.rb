ab58fef4765e48d150f26e55084b378ea41f707a
cbrenner04 <cbrenner265@gmail.com>
2019-12-14 17:31:00 -0600

    2019 day 13 part 2

--- 2019/13/solution.rb
+++ 2019/13/solution.rb
@@ -5,4 +5,9 @@ require_relative("../intcode.rb")
 program = INPUT.split(",").map(&:to_i)
 int_comp = Intcode.new(program)
 output, = int_comp.run
-p output
+count = 0
+output.each_slice(3) do |set|
+  tile_id = set[2]
+  count += 1 if tile_id == 2
+end
+puts count
