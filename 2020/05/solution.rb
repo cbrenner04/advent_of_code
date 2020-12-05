04e9056d5f03450e4227b42ec213a7a70238c452
cbrenner04 <cbrenner265@gmail.com>
2020-12-05 14:30:58 -0600

    Fancier

--- 2020/05/solution.rb
+++ 2020/05/solution.rb
@@ -3,7 +3,9 @@
 data = INPUT.each_line.map(&:chomp)
 
 # binary solution (only got to this after seeing others)
-ids = data.map { |d| d[0..6].tr("B", "1").tr("F", "0").to_i(2) * 8 + d[7..-1].tr("R", "1").tr("L", "0").to_i(2) }.sort
+ids = data.map { |d| d.gsub(/[BFRL]/, "B" => "1", "F" => "0", "L" => "0", "R" => "1").to_i(2) }.sort
+# # binary solution before realizing you can do away with the column * 8 + row, also go fancier with my text replacement
+# ids = data.map { |d| d.tr("B", "1").tr("F", "0").to_i(2) * 8 + d[7..-1].tr("R", "1").tr("L", "0").to_i(2) }.sort
 
 p ids.last
 p ((ids.first..ids.last).to_a - ids).first
