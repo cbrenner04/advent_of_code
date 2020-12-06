010531113ed939593357af6f5683ca2a95435d8c
cbrenner04 <cbrenner265@gmail.com>
2020-12-06 10:22:14 -0600

    2020 day 6

---
+++ 2020/06/solution.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+data = INPUT.split("\n\n").map(&:split)
+
+counts = data.map do |group|
+  answers = group.flat_map { |x| x.split("") }.uniq
+  all_count = answers.map { |a| group.all? { |g| g.include?(a) } ? 1 : 0 }.reduce(:+)
+  [answers.count, all_count]
+end
+
+p counts.map(&:first).reduce(:+)
+p counts.map(&:last).reduce(:+)
