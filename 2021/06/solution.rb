fb6c6ba9508e24e637f27d9215f37d21f17cd1d3
cbrenner04 <cbrenner265@gmail.com>
2021-12-07 06:04:04 -0500

    2021 Day 6 Part 1

---
+++ 2021/06/solution.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+INPUT = "3,4,3,1,2"
+fishes = INPUT.split(",").map(&:to_i)
+
+# it doesn't just double every 7 days
+
+# need to know how many fish have greater than 6 days before spawning
+# every 7 days and remove that before doubling
+
+# i really don't want to brute force this but my maths is not good
+
+# the brute force won't work on the second part
+
+# part one - brute force
+80.times do
+  lfishes = fishes.dup
+  lfishes.each_with_index do |fish, index|
+    if fish.zero?
+      fishes[index] = 6
+      fishes.push(8)
+    elsif fish.positive?
+      fishes[index] -= 1
+    else
+      throw "HOW DID YOU GET HERE?"
+    end
+  end
+end
+
+p fishes.count
