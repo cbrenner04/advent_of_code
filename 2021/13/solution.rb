1e95e23e1898bdbbf1c4b32eda6cd98e2449e051
Christopher Brenner <chris.brenner@tempus.com>
2021-12-17 17:41:41 -0500

    :rage:

--- 2021/13/solution.rb
+++ 2021/13/solution.rb
@@ -2,25 +2,34 @@
 
 dots, instructions = INPUT.split("\n\n").map { |a| a.split("\n") }
 dot_coords = dots.map { |d| d.split(",").map(&:to_i) }
-max_x = dot_coords.map(&:first).max
-max_y = dot_coords.map(&:last).max
-transparent_sheet = Array.new(max_y + 1) { Array.new(max_x + 1, ".") }
+max_x = dot_coords.map(&:first).max + 1
+max_y = dot_coords.map(&:last).max + 1
+transparent_sheet = Array.new(max_y) { Array.new(max_x, " ") }
 dot_coords.each { |x, y| transparent_sheet[y][x] = "#" }
+
 instructions.each_with_index do |instruction, index|
-  dir, line = instruction.scan(/fold along (x|y)=(\d+)/).first
+  # grab the direction of the fold and the line on which the fold takes place
+  dir, line = instruction.scan(/fold along (x|y)=(\d+)/).flatten.compact
   if dir == "x"
-    transparent_sheet.each_with_index.map do |l, l_index|
-      fold = l[line.to_i + 1..].reverse
+    # iterate through each line of the sheet
+    transparent_sheet.each_with_index do |_, l_index|
+      # copy the line from the fold line plus 1 (fold line is dropped) and reverse
+      fold = transparent_sheet[l_index][line.to_i + 1..].reverse
+      # keep only the first part of the line from the fold line minus 1 (fold line is dropped) on the sheet
+      transparent_sheet[l_index] = transparent_sheet[l_index][0..line.to_i - 1]
+      # go through the reversed second part of the line and set the dots
       fold.each_with_index do |space, space_index|
         next unless space == "#"
 
         transparent_sheet[l_index][space_index] = "#"
       end
-      transparent_sheet[l_index] = transparent_sheet[l_index][0..line.to_i - 1]
     end
   else
+    # copy the bottom part of the sheet from the fold line plus 1 (fold line is dropped) and reverse
     fold = transparent_sheet[line.to_i + 1..].reverse
+    # keep only the top part of the sheet from the fold line minus 1 (fold line is dropped)
     transparent_sheet = transparent_sheet[0..line.to_i - 1]
+    # go through reversed bottom part and set dots
     fold.each_with_index do |l, l_index|
       l.each_with_index do |space, space_index|
         next unless space == "#"
@@ -29,7 +38,9 @@ instructions.each_with_index do |instruction, index|
       end
     end
   end
+  # if this is the first pass, output the number of dots
   p transparent_sheet.map { |l| l.count("#") }.reduce(&:+) if index.zero?
 end
+
 # TODO: part two not quite correct
 transparent_sheet.each { |l| puts l.join }
